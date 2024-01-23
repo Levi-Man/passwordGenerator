857db06258700310836455fe7601f558b63b533e
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-01-23 15:33:03 -0500

    adding character inclusions

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,25 @@ function writePassword() {
 
 }
 
-function generatePassword
+function generatePassword() {
+  let length = prompt("Enter password length between 8 and 128 characters.");
+  if (!(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+  }
+
+  var includeLowerCase = confirm("Include lowercase characters?");
+  var includeUpperCase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  if (!includeLowerCase && !includeUpperCase && !includeNumeric && !includeSpecial) {
+    alert("Please select at least one character type inclusion.");
+    return "";
+  }
+
+  
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
